1faf20268c3d1f900a7edbf3ca18304e641c5879
Enes UTKU <ensutku@hotmail.com>
2024-06-14 22:07:20 +0300

    Initial Commit

---
+++ Simple Weather App Example/Simple_Weather_App_ExampleApp.swift
@@ -0,0 +1,12 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+@main
+struct Simple_Weather_App_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
