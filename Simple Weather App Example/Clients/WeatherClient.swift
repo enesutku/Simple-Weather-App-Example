0041aed1bbb708e4eab849e0e50cd288ccb15277
Enes UTKU <ensutku@hotmail.com>
2024-06-14 22:33:15 +0300

    Relase

---
+++ Simple Weather App Example/Clients/WeatherClient.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherClient.swift
+//  Weatherly
+//
+//  Created by Mohammad Azam on 11/2/23.
+//
+
+import Foundation
+
+struct WeatherClient {
+    
+    func fetchWeather(location: Location) async throws -> Weather {
+        
+        let (data, response) = try await URLSession.shared.data(from: APIEndpoint.endpointURL(for: .weatherByLatLon(location.lat, location.lon)))
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw NetworkError.invalidResponse
+        }
+        
+        let weatherResponse = try JSONDecoder().decode(WeatherResponse.self, from: data)
+        return weatherResponse.main 
+    }
+    
+}
---
+++ Simple Weather App Example/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Constants {
+    
+    struct Keys {
+        static let weatherAPIKey = "c11da5acdf5811e090bc4cee0cbc31d9"
+    }
+}
